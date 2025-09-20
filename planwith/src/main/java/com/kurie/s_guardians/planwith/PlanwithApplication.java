3088a6e9d7ab883529f01cfdfff013079629c294
김기범 <63234082+ejsjdj@users.noreply.github.com>
2025-09-20 21:01:59 +0900

    Spring Initializr

---
+++ planwith/src/main/java/com/kurie/s_guardians/planwith/PlanwithApplication.java
@@ -0,0 +1,13 @@
+package com.kurie.s_guardians.planwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanwithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanwithApplication.class, args);
+	}
+
+}
---
+++ planwith/src/test/java/com/kurie/s_guardians/planwith/PlanwithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurie.s_guardians.planwith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanwithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
